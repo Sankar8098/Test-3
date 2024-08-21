91f267d5bb72340b5c4efcc9da0fa50cf2e5f9a7
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-08-21 18:47:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Bot token from @BotFather
+BOT_TOKEN = os.getenv('BOT_TOKEN', 'your-bot-token-here')
+
+# Log channel ID
+LOG_CHANNEL_ID = os.getenv('LOG_CHANNEL_ID', '@your_log_channel')
