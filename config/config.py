fc115e6a786db876b3094d9a1467052096bcb4c2
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-08-21 19:01:00 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bot token from @BotFather
-BOT_TOKEN = os.getenv('BOT_TOKEN', 'your-bot-token-here')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7105677302:AAH5fwANVg-kPnKWZV-rqIy4j9Xk7YD4xOo')
 
 # Log channel ID
-LOG_CHANNEL_ID = os.getenv('LOG_CHANNEL_ID', '@your_log_channel')
+LOG_CHANNEL_ID = os.getenv('LOG_CHANNEL_ID', '@kolyees')
